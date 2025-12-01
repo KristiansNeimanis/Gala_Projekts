042e2ac99db0d912b6b14eb5b6432e4d2e715a52
Kristians <dummy@example.com>
2025-12-01 10:15:54 +0200

    Added Loading screen

--- Menu_stuff/Main_menu.gd
+++ Menu_stuff/Main_menu.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Dungeon_generation/dungeon_generator.tscn")
+	LoadsManager.load_scene("res://Dungeon_generation/dungeon_generator.tscn")
 
 
 func _on_quit_pressed():
