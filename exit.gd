bb161fb2ae942e38216ac5488a15551103bb894c
Kristians <dummy@example.com>
2026-05-04 00:45:04 +0300

    added path to exit scene

--- exit.gd
+++ exit.gd
@@ -2,11 +2,14 @@ extends Area3D
 
 @onready var collision_shape_3d = $CollisionShape3D
 @onready var win_text = $"../Player/UI/win_text"
+@onready var audio_stream_player_3d = $AudioStreamPlayer3D
+
 func enable_exit():
 	collision_shape_3d.disabled = false
 	self.visible = true
 
 func interacted_with():
 	win_text.visible = true
-	print("WIN")
-	self.queue_free()
+	audio_stream_player_3d.play()
+	await get_tree().create_timer(1.5).timeout
+	get_tree().change_scene_to_file("res://Scenes/Menu_stuff/lived.tscn")
